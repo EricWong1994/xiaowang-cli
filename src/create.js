42d2fcbc149b0b91c9659c1a574caab4a7392b51
搬砖达人小王git-flow <461864051@qq.com>
2022-09-29 09:06:04 +0800

    feat: fetchReopLists

--- src/create.js
+++ src/create.js
@@ -1,3 +1,13 @@
-module.exports = (projectName) => {
+const axios = require('axios');
+// 1).获取仓库列表
+const fetchReopLists = async () => {
+  // 获取当前组织中的所有仓库信息,这个仓库(https://github.com/lxy-cli)中存放的都是项目模板
+  const { data } = await axios.get('https://api.github.com/orgs/lxy-cli/repos'); return data
+};
+
+module.exports = async (projectName) => {
+  let repos = await fetchReopLists();
+  repos = repos.map((item) => item.name);
+  console.log(repos);
   console.log(`此处是文件${projectName}`);
-}
+};
