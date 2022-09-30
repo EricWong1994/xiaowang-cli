2fc1604fa070b1194eb89fe6cbedc8c655460d47
搬砖达人小王git-flow <461864051@qq.com>
2022-09-30 09:05:57 +0800

    feat: 获取版本信息

--- src/create.js
+++ src/create.js
@@ -1,29 +1,29 @@
-const axios = require('axios');
 const inquirer = require('inquirer')
-const { fnLoadingByOra } = require('./utils/common.js');
+const { fnLoadingByOra, fetchReopLists, getTagLists } = require('./utils/common');
 
-// 1).获取仓库列表
-const fetchReopLists = async () => {
-  // 获取当前组织中的所有仓库信息,这个仓库(https://github.com/lxy-cli)中存放的都是项目模板
-  const { data } = await axios.get('https://api.github.com/orgs/lxy-cli/repos');
-  return data;
-};
 
 module.exports = async (projectName) => {
-  let repos = await fnLoadingByOra(fetchReopLists, '正在链接你的仓库...');
+  let repos = await fnLoadingByOra(fetchReopLists, '正在链接你的仓库...')();
   repos = repos.map((item) => item.name);
-  console.log(repos);
-  console.log(`此处是文件${projectName}`);
-  inquirer.prompt([
+  // 使用inquirer 在命令行中可以交互
+  const { repo } = await inquirer.prompt([
     {
       type: 'list',
       name: 'repo',
       message: '请选择一个你要创建的项目',
       choices: repos
     }
-  ]).then((answers) => {
-    console.log('结果为:')
-    console.log(answers)
-  })
-};
+  ]);
+  let tags = await fnLoadingByOra(getTagLists, `正在链接你的选择的仓库${repo}的版本号...`)(repo);
+  tags = tags.map((item) => item.name);
+  // console.log(`仓库 ${repo}的版本信息列表：${tags}`);
 
+  const { tag } = await inquirer.prompt([{
+    type: 'list',
+    name: 'tag',
+    message: '请选择一个该项目的版本下载',
+    choices: tags
+  }]);
+  console.log(`我现在选择了哪个仓库: ${repo}`);
+  console.log(`我现在选择了哪个版本: ${tag}`);
+}
--- src/utils/common.js
+++ src/utils/common.js
@@ -1,4 +1,6 @@
 const ora = require('ora')
+const axios = require('axios');
+
 // 根据我们想要实现的功能配置执行动作，遍历产生对应的命令
 const mapActions = {
   create: {
@@ -24,15 +26,29 @@ const mapActions = {
 }
 
 // 封装loading效果
-const fnLoadingByOra = async (fn, message) => {
+const fnLoadingByOra = (fn, message) => async (...argv) => {
   const spinner = ora(message);
   spinner.start();
-  let result = await fn();
+  let result = await fn(...argv);
   spinner.succeed(); // 结束loading
   return result;
+}//  获取仓库(repo)的版本号信息
+
+const getTagLists = async (repo) => {
+  const { data } = await axios.get(`https://api.github.com/repos/lxy-cli/${repo}/tags`);
+  return data;
 }
+
+// 获取仓库列表
+const fetchReopLists = async () => {
+  // 获取当前组织中的所有仓库信息,这个仓库(https://github.com/lxy-cli)中存放的都是项目模板
+  const { data } = await axios.get('https://api.github.com/orgs/lxy-cli/repos');
+  return data;
+};
 
 module.exports = {
   mapActions,
-  fnLoadingByOra
+  fnLoadingByOra,
+  fetchReopLists,
+  getTagLists
 };
